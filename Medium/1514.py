4a4f89158eaa96601ad92b1532347310fa147b30
Tsangington <harry.tsang@code.berlin>
2023-06-28 17:53:29 +0200

    Day 3 + Problem 373 Revision

---
+++ Medium/1514.py
@@ -0,0 +1,4 @@
+#Day 3 | 1514. Path with Maximum Probability
+#Use a node path algorithm, then find probabilities of paths, and compare.
+
+#WIP
--- Medium/373.py
+++ Medium/373.py
@@ -1,3 +1,4 @@
+"""
 def kSmallestPairs(nums1, nums2, k):
     #create list of all possible outcomes
     pairList = []
@@ -11,7 +12,33 @@ def kSmallestPairs(nums1, nums2, k):
     pairList = pairList[0:k]
     return pairList
 
+#WIP: Memory Limit Exceeded, 25/35
+
+Idea 2: Instead of listing all possible outcomes, calculate sum of arrays,
+  and only add to the array if len array is not 3, or smaller than sum.
+"""
+def kSmallestPairs(nums1, nums2, k):
+    pairList = []
+    maxPairSum = 0
+    for x in nums1:
+        for y in nums2:
+            total = x+y
+            if len(pairList) >= k: #bigger than k means make a check. 
+                if total <= maxPairSum:
+                    pairList.append([x,y])
+            else: #less than 3, find sum of number 
+                pairList.append([x,y])
+                if total > maxPairSum:
+                    maxPairSum = total #change the maxPairSum accordingly so other pairs can be checked.
+                                     #When pairList is len 3, this variable should have the biggest sum.
+    #sorting
+    pairList = sorted(pairList, key=sum)
+    #splice list according to num k
+    pairList = pairList[0:k]
+    return pairList
+
 print(kSmallestPairs(nums1 = [1,7,11], nums2 = [2,4,6], k = 3))
 print(kSmallestPairs(nums1 = [1,1,2], nums2 = [1,2,3], k = 2))
 
-#WIP: Memory Limit Exceeded, 25/35
+#WIP
+#New version works, but still exceeds memory limit. Would need to make it change maxSumPair when a smaller array sum is added.
