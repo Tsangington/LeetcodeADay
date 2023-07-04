5f1dfe1762a9bddd836ff3934d3f51f4d47ecd68
Tsangington <harry.tsang@code.berlin>
2023-07-04 19:01:59 +0200

    Day4 1768

---
+++ Easy/1768.py
@@ -0,0 +1,28 @@
+"""
+1768. Merge Strings Alternately
+
+You are given two strings word1 and word2. Merge the strings by adding letters 
+in alternating order, starting with word1. If a string is longer than the other, 
+append the additional letters onto the end of the merged string.
+
+Return the merged string.
+"""
+
+def mergeAlternately(word1, word2):
+    # find min len
+    mergedString = ""
+    if len(word1) < len(word2):
+        iterations = len(word1)
+        endstring = word2[len(word1):]
+    else:
+        iterations = len(word2)
+        endstring = word1[len(word2):]
+
+    for index in range(iterations):
+        mergedString += word1[index] + word2[index]
+    mergedString += endstring
+
+    return (mergedString)
+    
+
+print(mergeAlternately(word1 = "abcd", word2 = "pq"))
